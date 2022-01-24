06e5c800a011d70ee5b878fc0354fd624317cf11
DaisukeKogasaki <kgsk516@gmail.com>
2022-01-25 02:16:05 +0900

    textareaに入力した文字列をQRコードに変換する機能を実装

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <template>
+  <div id="qr-code">
+    <qrcode-vue :value="value" :size="size" level="H" render-as="svg" />
+  </div>
+  <div>変換内容</div>
+  <div><textarea type="textarea" id="qr-val" /></div>
+  <div><button @click="generate()">変換</button></div>
 </template>
 
 <script>
+  import QrcodeVue from 'qrcode.vue'
+
+  export default {
+    data() {
+      return {
+        value: 'https://example.com',
+        size: 300,
+      }
+    },
+    components: {
+      QrcodeVue,
+    },
+    methods: {
+      generate: function() {
+        let qrElm = document.getElementById("qr-val");
+        this.value = qrElm.value
+      }
+    },
+  }
+
 </script>
 
-<style>
+<style scoped>
 </style>
