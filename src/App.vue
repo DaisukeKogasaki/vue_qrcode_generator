87a36e88606f5bc7d0d6da9c6137ccd620e79950
Daisuke Kogasaki <kgsk516@gmail.com>
2022-04-10 00:26:22 +0900

    QRコードを画像ファイルでダウンロードできるようにした
    vscodeでコードのフォーマットを調整

--- src/App.vue
+++ src/App.vue
@@ -2,62 +2,95 @@
   <v-app>
     <v-main>
       <v-container>
-      <v-row>
-        <v-col>
-          <qrcode-vue :value="value" :size="size" level="H" render-as="svg" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-textarea id="qr-val" placeholder="生成内容" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-btn @click="generate()" block color="blue" text-color="white" >変換</v-btn>
-        </v-col>
-      </v-row>
+        <v-row>
+          <v-col>
+            <qrcode-vue
+              :value="value"
+              :size="size"
+              level="H"
+              render-as="svg"
+              id="qr-code"
+            />
+            <qrcode-vue
+              :value="value"
+              :size="size"
+              level="H"
+              render-as="png"
+              id="qr-code-as-image"
+              style="visibility: hidden; position: absolute"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-textarea id="qr-val" placeholder="生成内容" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn @click="generate()" block text-color="white" color="#2196F3"
+              ><v-icon dark left> mdi-qrcode-edit </v-icon>変換</v-btn
+            >
+          </v-col>
+          <v-col>
+            <v-btn
+              @click="downloadItem()"
+              block
+              text-color="white"
+              color="#3F51B5"
+              ><v-icon dark left> mdi-download </v-icon>PNGで保存</v-btn
+            >
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-  import QrcodeVue from 'qrcode.vue'
+import QrcodeVue from "qrcode.vue";
 
-  export default {
-    data() {
-      return {
-        value: 'https://daisukekogasaki.github.io/vue_qrcode_generator/',
-        size: 300,
-      }
+export default {
+  data() {
+    return {
+      value: "https://daisukekogasaki.github.io/vue_qrcode_generator/",
+      size: 300,
+    };
+  },
+  components: {
+    QrcodeVue,
+  },
+  methods: {
+    generate: function () {
+      let qrElm = document.getElementById("qr-val");
+      this.value = qrElm.value;
     },
-    components: {
-      QrcodeVue,
+    downloadItem() {
+      var link = document.createElement("a");
+      link.download = "qrcode.png";
+      link.href = document.getElementById("qr-code-as-image").toDataURL();
+      link.click();
     },
-    methods: {
-      generate: function() {
-        let qrElm = document.getElementById("qr-val");
-        this.value = qrElm.value
-      }
-    },
-  }
-
+  },
+};
 </script>
 
 <style scoped>
-  span, svg, textarea, button {
-    display: block;
-    margin-right: auto;
-    margin-left: auto;
-  }
+span,
+svg,
+textarea,
+button {
+  display: block;
+  margin-right: auto;
+  margin-left: auto;
+}
 
-  span {
-    text-align: center;
-  }
+span {
+  text-align: center;
+}
 
-  textarea, button {
-    width: 300px;
-  }
-
+textarea,
+button {
+  width: 300px;
+}
 </style>
